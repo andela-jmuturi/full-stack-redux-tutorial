21a836768b2f22be24f8bd08d07225a1433357cb
James Muturi <james.muturi@andela.com>
2016-05-13 11:50:14 +0300

    Handle voting on client side and use action creators
    Votes cast are now synchronized across all clients
    in real time.

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -2,32 +2,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import io from 'socket.io-client';
 
 import reducer from './reducer';
+import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
+
 import App from './components/App';
 import { VotingContainer } from './components/Voting';
 import { ResultsContainer } from './components/Results';
-
-const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
+/* eslint-enable no-unused-vars */
 
 const socket = io(
   `${window.location.protocol}//${window.location.hostname}:8090`);
 
 socket.on('state', (state) => {
-  store.dispatch({type: 'SET_STATE', state});
+  store.dispatch(setState(state));
 });
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
 const routes =
   <Route component={App}>
